ef38453cf35f16e750ebd8af9c3e30f4e6b20260
Matt Ludwig <mattludwig@Matts-MacBook-Pro.local>
2016-09-12 08:48:11 -0400

    first commit

---
+++ PairOfStrings.java
@@ -0,0 +1,9 @@
+public class PairOfStrings{ private String string1; private String string2;
+
+public PairOfStrings(String s1, String s2){
+//Implement
+}
+
+public String toString() {
+    … //Implement
+} }
